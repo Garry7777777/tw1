a8b75fe64aeff1328123c11d72d5fb7396b67805
garry <garry4978@gmail.com>
2023-01-13 17:13:50 +0300

    1

---
+++ src/MyLinkedList.java
@@ -0,0 +1,44 @@
+public class MyLinkedList<T> implements MyList<T> {
+
+
+
+    public class Node<T>{
+
+        private Node<T> item;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node() {
+
+        }
+
+
+    }
+
+
+
+    @Override
+    public void add(T t) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
